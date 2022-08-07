679b88702e94e6dd44f747205db476697fc3cc57
GrandSir <69051988+GrandSir@users.noreply.github.com>
2022-08-07 13:22:32 +0300

    fix responsive design

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -9,8 +9,8 @@ const gradients = [
     "from-green-400 to-blue-600",
     "from-cyan-500 to-blue-500",
     "from-purple-600 to-blue-500",
-   // "from-teal-300 to-lime-300",
-   // "from-red-200 via-red-300 to-yellow-200"
+    "from-teal-300 to-lime-300",
+    "from-red-200 via-red-300 to-yellow-200"
 ]
 const PostCard = (post : Post) => {
     console.log(post)
@@ -22,25 +22,28 @@ const PostCard = (post : Post) => {
                     <span className="text-blue-700">Yazar Önerisi</span> 
                 </div>
             </div>
-            <div className="grid relative overflow-hidden shadow-md pb-80 mb-5 object-fit place-items-end rounded-lg py-10">
+            <div className="relative mb-7 mt-3 mx-2 lg:mx-0">
                 <img
                     src={post.node.featuredImage.url}
                     alt={post.node.title}
-                    className="object-fit absolute w-full shadow-lg rounded-lg"
+                    height={"440"}
+                    width={"900"}
+                    className="shadow-lg rounded-lg"
                 />
                 <div className="absolute bottom-0 left-0 ml-1">
                     { 
                     post.node.categories.map((category, index) => 
-                    <div className={`relative ml-2 inline-flex shadow-xl p-0.5 mb-4 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br ${gradients[Math.floor(Math.random()*gradients.length)]}`}>                    <span className="relative px-3 py-2.5 transition-all ease-in duration-75 shadow-2xl rounded-full font-semibold ">
-                        {category.name}
-                    </span>
+                    <div className={`cursor-pointer hover:scale-105 relative ml-2 inline-flex shadow-xl p-0.5 mb-4 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br ${gradients[Math.floor(Math.random()*gradients.length)]}`}>                    
+                    <Link href={`/kategori/${category.slug}`}>
+                        <span className="relative px-3 py-2.5 transition-all ease-in duration-75 shadow-2xl rounded-full font-semibold">
+                            {category.name}
+                        </span>
+                    </Link>
                     </div>
                     )
                     }
                     </div>
             </div>
-
-
             <h1 className="transition duration-100 text-center mb-4 cursor-pointer hover:text-blue-600 text-3xl font-semibold">
                 <Link href={`/post/${post.node.slug}`}>
                     {`${post.node.title}`} 
@@ -56,32 +59,32 @@ const PostCard = (post : Post) => {
                 </button>
             </div>
         </div>
-          <div className="bg-[#ededed] block lg:flex text-center items-center justify-between px-4 mb-3 w-full rounded-b-lg p-5">
-          {
-          post.node.authors.map((author, index) => (
-              <div className="flex justify-center mb-7 lg:mb-0 w-full lg:w-auto mr-8">
-                  <img
-                      alt={author.bio}
-                      width= {"50"}
-                      height={"50"}
-                      className="align-middle rounded-full object-scale-down mr-4"
-                      src= {author.photo.url}
-                  />
-                  <p className="flex flex-col">
-                      <span className="font-semibold">
-                          {author.name}
-                      </span>
-                      <span className="text-left font-thin">
-                          {toLocalizedAuthor(post.node.roles[index])}
-                      </span>
-                  </p>
-              </div>
-          )
-          )
-          }
-  </div>
-  </div>
+        <div className="bg-[#ededed] block lg:flex lg:justify-between md:justify-left lg:mb-3 w-full rounded-b-lg px-5 pt-3 lg:p-5">
+        {
+        post.node.authors.map((author, index) => (
+            <div className="flex lg:justify-center md:justify-left w-full lg:w-auto lg:mr-8">
+                <img
+                    alt={author.bio}
+                    width= {"50"}
+                    height={"50"}
+                    className="align-middle rounded-full object-scale-down mb-4 lg:mb-0 mr-4"
+                    src= {author.photo.url}
+                />
+                <p className="flex flex-col">
+                    <span className="font-semibold">
+                        {author.name}
+                    </span>
+                    <span className="text-left font-thin">
+                        {toLocalizedAuthor(post.node.roles[index])}
+                    </span>
+            </p>
+            </div>
+        )
     )
+}
+</div>
+</div>
+)
 }
 
 export default PostCard 
