4951f28e5e053862fa190447725e6e5e44a1842d
GrandSir <69051988+GrandSir@users.noreply.github.com>
2022-08-19 09:11:02 +0300

    fix bad typescript structure

--- components/Models.tsx
+++ components/Models.tsx
@@ -1,56 +1,55 @@
 export type Post = {
-    node: PostNode;
-}
+  node: PostNode;
+};
 
 export type Feylesof = {
-    node: FeylesofNode
-}
+  node: FeylesofNode;
+};
 
 export type Category = {
-    name: string; 
-    slug: string;
-    iconPath: string;
-    timesClicked: number;
-}
+  name: string;
+  slug: string;
+  iconPath: string;
+  timesClicked: number;
+};
 
 export type FeylesofNode = {
-    bio: string;
-    id: string;
-    slug: string;
-    name: string;
-    photo: Photo;
-    feylesofType: string;
-    followers: Feylesof[]
-    following: Feylesof[]
-}
+  bio: string;
+  id: string;
+  slug: string;
+  name: string;
+  photo: Photo;
+  followers: Feylesof[];
+  following: Feylesof[];
+  feylesofType: string;
+};
 
-type PostNode = {
-    title: string;
-    excerpt: string;
-    featuredImage: featuredImage;
-    slug: string;
-    categories: Category[];
-    feylesoflar: FeylesofNode[];
-    roles: string[];
-}
+export type PostNode = {
+  title: string;
+  excerpt: string;
+  featuredImage: featuredImage;
+  slug: string;
+  categories: Category[];
+  feylesoflar: FeylesofNode[];
+  roles: string[];
+};
 
-type featuredImage = {  
-    url: string;
-}
+type featuredImage = {
+  url: string;
+};
 
 type Photo = {
-    url: string;
-}
+  url: string;
+};
 
-const AuthorRole = {
-    "lead" : "Yazar",
-    "second" : "İkinci Yazar", 
-    "collabrative" : "Ortak Yazar",
-    "guest" : "Misafir Yazar",
-    "editor" : "Editör"
-}
+const AuthorRole = new Map<string, string>([
+  ["lead", "Yazar"],
+  ["second", "İkinci Yazar"],
+  ["collabrative", "Ortak Yazar"],
+  ["guest", "Misafir Yazar"],
+  ["editor", "Editör"],
+]);
 
-export function toLocalizedAuthor(arg: string) : string {
-    //@ts-ignore
-    return AuthorRole[arg]
+export function toLocalizedAuthor(arg: string): string {
+  return AuthorRole.get(arg)!;
 }
