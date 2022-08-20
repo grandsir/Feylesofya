7ae357db584c9a91b8b310fd717993decf41d656
GrandSir <d.mehmet.atabey@gmail.com>
2022-08-20 19:54:17 +0300

    add features to PostDetail Screen

--- components/PostDetail.tsx
+++ components/PostDetail.tsx
@@ -3,19 +3,17 @@ import React from 'react';
 import moment from 'moment';
 import {Post} from "../services/models";
 import { RichText } from '@graphcms/rich-text-react-renderer';
-import Link from "next/link";
+import  Link from "next/link";
 import {toLocalizedAuthor} from "../scripts";
+import Image from 'next/image';
 
 const PostDetail = ( post : Post) => {
     return (
-    <div className="text-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8">
-       <div>
-            <div className="relative overflow-hidden shadow-lg mb-6 max-w-3xl mx-auto">
-                <img src={post.featuredImage.url} alt="" className="object-top h-full object-cover shadow-lg rounded-t-lg lg:rounded-lg" />
-            </div>
-       </div>
-        <div className="px-4 max-w-3xl lg:px-0 mx-auto">
-            <div className="sm:flex justify-between mx-auto gap-x-10  md:justify-left lg:mb-3 w-full rounded-b-lg px-5 pt-3 lg:p-5">
+    <div className="text-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8 bg-none selection:bg-emerald-800">
+       <div className = "max-w-3xl mx-auto">
+        <img src={post.featuredImage.url} alt="" className="mx-auto object-top h-full object-cover shadow-lg rounded-t-lg lg:rounded-lg mb-8"/>
+        <div className="px-4 lg:px-0 mx-auto">
+            <div className="sm:flex justify-between mx-auto gap-x-10 md:justify-left lg:mb-3 w-full rounded-b-lg px-5 pt-3 lg:p-5">
                 {post.feylesoflar.map((feylesof, index) => (
                     <div>
                         <Link
@@ -50,10 +48,26 @@ const PostDetail = ( post : Post) => {
                 content={post.content.raw}
                 renderers={
                     {
-                        p: ({children}) => <p> {children} </p>
+                        p: ({children}) => <p className="mb-8 bg-none selection:bg-emerald-800"> {children} </p>,
+                        bold: ({children}) => <span className = "text-emerald-600 selection:text-black font-semibold">{children}</span>,
+                        h2: ({children}) => <h2 className = "text-center my-8 text-2xl">{children}</h2>,
+                        img: ({ src, altText, height, width }) =>
+                            <div className = "my-8 ">
+                                <Image className= "shadow-2xl rounded-xl"
+                                       src={src ?? ""}
+                                       alt={altText}
+                                       height={height}
+                                       width={width}
+                                       objectFit="cover"
+                                />
+                            </div>,
+                        ul: ( {children}) => <ul className = "space-y-4 max-w-md list-disc list-inside marker:text-yellow-500 mt-8"> { children }</ul>,
+                        li:  ( {children}) => <li className = "text-indigo-300 hover:text-yellow-500 cursor-pointer"> { children }</li>,
+                        h3:  ( {children}) => <h3 className = "text-xl text-center mt-6">{ children }</h3>
                     }
                 }
             />
+         </div>
         </div>
     </div>
     );
